fcf14a0862c2aed2531730f30dc3ed3bd10b80c7
vanytas <vanytas@vanytasui-MacBookPro.local>
2024-09-16 21:54:29 +0900

    first commit

---
+++ src/main/java/site/keebground/KeebGroundApplication.java
@@ -0,0 +1,13 @@
+package site.keebground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeebGroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeebGroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/keebground/KeebGroundApplicationTests.java
@@ -0,0 +1,13 @@
+package site.keebground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeebGroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
